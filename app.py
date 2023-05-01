d32859720ed17d2b52c72b4bc83fdf595b4f10c2
Eduardo Chemalle <eduardochemalle@MacBook-Air-de-Eduardo.local>
2023-05-01 14:39:14 -0300

    tirando strealit

--- app.py
+++ app.py
@@ -89,13 +89,13 @@ fig_tasks_by_hour.update_layout(
 st.plotly_chart(fig_tasks_by_hour)
 
 
-# hide_st_style = """"
-#                 <style>
-#                 #MainMenu {visibility: hiden;}
-#                 footer {visibility: hidden;}
-#                 header {visibility: hidden;}
-#                 </style>
-# """
+hide_st_style = """"
+                <style>
+                #MainMenu {visibility: hiden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+"""
 
 
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
